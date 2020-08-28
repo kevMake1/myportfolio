a90c62be262bb12dff35a2cf08b8033737a9b945
Kevaughn Clarke <kevaughnclarke@MacBook-Pro.local>
2020-08-28 17:33:17 -0400

    added smooth scrolling

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link, animateScroll as scroll } from "react-scroll";
 
 import './Nav.css'
 
@@ -7,14 +8,28 @@ export default function Nav() {
         <nav className='Nav'>
             {/* <div className='Logo'>Logo</div> */}
             <ul>
-                <a href="#Bio"><li>About Me</li></a>
-                <a href="#projects"><li>My Projects</li></a>
+                <Link
+                    activeClass="active"
+                    to="Bio"
+                    spy={true}
+                    smooth={true}
+                    offset={-70}
+                    duration={500}
+                ><a href="#Bio"><li>About Me</li></a></Link>
+                <Link
+                    activeClass="active"
+                    to="projects"
+                    spy={true}
+                    smooth={true}
+                    offset={-70}
+                    duration={500}
+                ><a href="#projects"><li>My Projects</li></a></Link>
             </ul>
-        </nav>        
+        </nav>
     )
 }
 
 
-{/* <Link to='/'><li>Home</li></Link>
+            {/* <Link to='/'><li>Home</li></Link>
 <Link to='/social'><li>Social</li></Link>
 <Link to='/signup'><li>Signup</li></Link> */}
